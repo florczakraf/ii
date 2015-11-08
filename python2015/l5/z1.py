bff5367d09e08d12263e4c275d590bbb2dd39558
Rafal Florczak <florczakraf@gmail.com>
2015-11-08 19:24:34 +0100

    python l5z1 exception handling

--- python2015/l5/z1.py
+++ python2015/l5/z1.py
@@ -10,29 +10,33 @@ class Traversal:
     self.visited = set()
 
   def traverse(self, url, depth = 1):
-    page = urlopen(url).read().decode('utf-8')
-    
+    try:
+      page = urlopen(url).read().decode('utf-8')
+    except:
+      print("There was an error while processing: %s" % url)
+      return
+
     if depth == 1:
       self.visited.add(url)
-    
+
     if depth < self.max_depth:
       expr = re.compile("http(s){0,1}:\/\/([a-zA-Z0-9\-]+\.)*[a-zA-Z]+(\/([a-zA-Z0-9\-]*\.*\=*\?*)*)*")
       for match in expr.finditer(page):
         link = match.group()
         if link not in self.visited:
-          self.visited.add(link)
-          self.queue.put((link, depth + 1))
-    
+          self.visited.add_nowait(link)
+          self.queue.put_nowait((link, depth + 1))
+
     for match in self.func(page):
       yield match
 
     if not self.queue.empty():
-      first = self.queue.get()
+      first = self.queue.get_nowait()
       for i in self.traverse(first[0], first[1]):
         yield i
     else:
-      return StopIteration()
-    
+      raise StopIteration()
+
 if __name__ == "__main__":
   def f (page):
     expr = re.compile("[\w ]*Python(\w*,* *)*\.")
@@ -43,11 +47,17 @@ if __name__ == "__main__":
   iter = tr.traverse("http://rflorczak.eu/python/index.html")
   for i in iter:
     print(i)
-    
+
   print('----')
-  
+
   tr2 = Traversal(2, f)
   iter2 = tr2.traverse("http://rflorczak.eu/python/1.html")
   for i in iter2:
     print(i)
-  
+
+  print('----')
+
+  tr3 = Traversal(5, f)
+  iter3 = tr3.traverse("http://rflorczak.eu/python/error.html")
+  for i in iter3:
+    print(i)
