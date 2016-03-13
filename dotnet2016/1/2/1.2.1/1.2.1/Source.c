a45a75574918e0f14ab265f9e6fb2c654db2f4c7
Rafał Florczak <florczak.raf@gmail.com>
2016-03-13 01:34:30 +0100

    dotnet 1.2-1,2

---
+++ dotnet2016/1/2/1.2.1/1.2.1/Source.c
@@ -0,0 +1,51 @@
+﻿/*
+ * Problemy:
+ * przy próbie drukowania: SE_ERR_NOASSOC --> There is no application associated with the given file name extension. This error will also be returned if you attempt to print a file that is not printable.
+ */
+
+#include <windows.h>
+#include <Strsafe.h>
+#include <Shlobj.h> // CSIDL_DESKTOPDIRECTORY
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+  DWORD dwWrote;
+  HANDLE hFile;
+
+  char sTime[22];
+  SYSTEMTIME time;
+
+  GetSystemTime(&time);
+  GetDateFormat(LOCALE_SYSTEM_DEFAULT, 0, &time, TEXT("dd-MM-yyyy"), sTime, 22);
+
+  WCHAR path[MAX_PATH];
+
+  SHGetSpecialFolderPath(HWND_DESKTOP, path, CSIDL_DESKTOPDIRECTORY, FALSE);
+  
+  StringCchCat(path, 200, TEXT("\\test.txt"));
+
+  hFile = CreateFile(path, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, 0, NULL);
+  if (hFile == INVALID_HANDLE_VALUE)
+  {
+    MessageBox(NULL, TEXT("Couldn't create file"), TEXT("Error"), 0);
+    PostQuitMessage(0);
+  }
+
+  if (!WriteFile(hFile, sTime, 22, &dwWrote, NULL))
+  {
+    MessageBox(NULL, TEXT("Couldn't write to file"), TEXT("Error"), MB_ICONEXCLAMATION);
+    PostQuitMessage(0);
+  }
+
+  int result = ShellExecute(0, "print", path, NULL, NULL, 0);
+
+  if (result)
+  {
+    WCHAR buf[64];
+    swprintf_s(buf, 64, TEXT("ShellExecute error while printing: %d"), result);
+
+    MessageBox(0, buf, 0, 0);
+  }
+ 
+  CloseHandle(hFile);
+}
---
+++ dotnet2016/1/2/1.2.2/1.2.2/Source.c
@@ -0,0 +1,96 @@
+﻿#include <windows.h>
+#include <Strsafe.h>
+
+HKEY hkSoftware, hkSettings;
+DWORD dwDisposition;
+DWORD width = 0;
+DWORD height = 0;
+
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
+
+char szClassName[] = "hjmjt6uhjfthujsdf";
+
+int WINAPI WinMain(HINSTANCE hThisInstance, HINSTANCE hPrevInstance, LPSTR lpszArgument, int nFunsterStil)
+{
+  HWND hwnd;
+  MSG messages;        
+  WNDCLASSEX wincl;       
+
+  wincl.hInstance = hThisInstance;
+  wincl.lpszClassName = szClassName;
+  wincl.lpfnWndProc = WindowProcedure;   
+
+  wincl.style = CS_DBLCLKS;
+  wincl.cbSize = sizeof(WNDCLASSEX);
+
+  wincl.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+  wincl.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+  wincl.hCursor = LoadCursor(NULL, IDC_ARROW);
+  wincl.lpszMenuName = NULL;
+  wincl.cbClsExtra = 0;
+  wincl.cbWndExtra = 0;
+
+  wincl.hbrBackground = (HBRUSH)GetStockObject(LTGRAY_BRUSH);
+
+  if (!RegisterClassEx(&wincl)) return 0;
+  
+  RegOpenKeyEx(HKEY_CURRENT_USER, "Software", 0, KEY_ALL_ACCESS, &hkSoftware);
+
+  RegCreateKeyEx(hkSoftware, "Programowanie pod Windows", 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &hkSettings, &dwDisposition);
+
+  if (dwDisposition == REG_CREATED_NEW_KEY)
+  {
+    MessageBox(0, 0, 0, 0);
+    width = 200;
+    height = 200;
+    RegSetValueEx(hkSettings, "Width", 0, REG_DWORD, (BYTE *) &width, sizeof(width));
+    RegSetValueEx(hkSettings, "Height", 0, REG_DWORD, (BYTE *) &height, sizeof(height));
+  }
+
+  DWORD dwSize = sizeof(DWORD);
+  RegQueryValueEx(hkSettings, "Width", NULL, NULL, (LPBYTE) &width, &dwSize);
+  RegQueryValueEx(hkSettings, "Height", NULL, NULL, (LPBYTE) &height, &dwSize);
+
+  hwnd = CreateWindowEx(
+    0,
+    szClassName,
+    TEXT("1.2.2"),
+    WS_OVERLAPPEDWINDOW,
+    CW_USEDEFAULT,
+    CW_USEDEFAULT,
+    width,
+    height,
+    HWND_DESKTOP,
+    NULL,
+    hThisInstance,
+    NULL
+    );
+
+  ShowWindow(hwnd, nFunsterStil);
+  while (GetMessage(&messages, NULL, 0, 0))
+  {
+    TranslateMessage(&messages);
+    DispatchMessage(&messages);
+  }
+
+  return messages.wParam;
+}
+
+LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+  switch (message)
+  {
+  case WM_SIZE:
+    width = LOWORD(lParam);
+    height = HIWORD(lParam);
+    RegSetValueEx(hkSettings, "Width", 0, REG_DWORD, (BYTE *) &width, sizeof(width));
+    RegSetValueEx(hkSettings, "Height", 0, REG_DWORD, (BYTE *) &height, sizeof(height));
+    break;
+  case WM_DESTROY:
+    PostQuitMessage(0);
+    break;
+  default:
+    return DefWindowProc(hwnd, message, wParam, lParam);
+  }
+  return 0;
+}
