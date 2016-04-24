592fee952bf1d09b3663c987dc34029d4e311ecb
Rafał Florczak <florczak.raf@gmail.com>
2016-04-24 13:03:33 +0200

    poo l6 1-2

---
+++ poo2016/l6/z1/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ILogger logger1 = LoggerFactory.Instance.GetLogger(LogType.File, Path.GetTempFileName());
+      logger1.Log("foo bar");
+
+      ILogger logger2 = LoggerFactory.Instance.GetLogger(LogType.None);
+      logger2.Log("bar bar");
+
+      ILogger logger3 = LoggerFactory.Instance.GetLogger(LogType.Console);
+      logger3.Log("qux");
+
+      Console.ReadKey();
+
+    }
+
+    public interface ILogger
+    {
+      void Log(string message);
+    }
+
+    public class FileLogger : ILogger
+    {
+      private string _path;
+
+      public void Log(string message)
+      {
+        using (StreamWriter sw = new StreamWriter(this._path, true))
+        {
+          sw.WriteLine(message);
+        }
+      }
+
+      public FileLogger(string path)
+      {
+        this._path = path;
+      }
+    }
+
+    public class ConsoleLogger : ILogger
+    {
+      public void Log(string message)
+      {
+        Console.WriteLine(message);
+      }
+    }
+
+    public class NullObject : ILogger
+    {
+      public void Log(string message)
+      {
+
+      }
+    }
+
+    public enum LogType { None, Console, File };
+
+    public class LoggerFactory
+    {
+      private static LoggerFactory _instance = null;
+
+      public ILogger GetLogger(LogType logType, string arg = null)
+      {
+        switch (logType)
+        {
+          case LogType.None:
+            return new NullObject();
+          case LogType.Console:
+            return new ConsoleLogger();
+          case LogType.File:
+            return new FileLogger(arg);
+          default:
+            return new NullObject();
+        }
+      }
+
+      public static LoggerFactory Instance
+      {
+        get
+        {
+          if (_instance == null)
+          {
+            lock (typeof(LoggerFactory))
+            {
+              _instance = new LoggerFactory();
+            }
+          }
+
+          return _instance;
+        }
+      }
+    }
+
+  }
+}
---
+++ poo2016/l6/z2/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Context context = new Context();
+      context.SetValue("x", false);
+      // (x && true) || (false || !y)
+      AbstractExpression exp = new Or(new And(new Var("x"), new Const(true)), new Or(new Const(false), new Not(new Var("y"))));
+
+      try
+      {
+        bool value = exp.Interpret(context);
+        Console.WriteLine("Value: {0}", value);
+      }
+      catch (KeyNotFoundException)
+      {
+        Console.WriteLine("[!] Unbound variable");
+      }
+
+      context.SetValue("y", true);
+
+      try
+      {
+        bool value = exp.Interpret(context);
+        Console.WriteLine("Value of expression: {0}", value);
+      }
+      catch (KeyNotFoundException)
+      {
+        Console.WriteLine("[!] Unbound variable");
+      }
+
+      Console.ReadKey();
+    }
+
+    public class Context
+    {
+      private Dictionary<string, bool> _dict = new Dictionary<string, bool>();
+
+      public bool GetValue(string var)
+      {
+        if (this._dict.ContainsKey(var))
+          return this._dict[var];
+        else throw new KeyNotFoundException();
+      }
+
+      public void SetValue(string var, bool val)
+      {
+        if (!this._dict.ContainsKey(var))
+          this._dict.Add(var, val);
+        else
+          this._dict[var] = val;
+      }
+    }
+
+    public abstract class AbstractExpression
+    {
+      public abstract bool Interpret(Context context);
+    }
+
+    public class Var : AbstractExpression
+    {
+      private string _name;
+
+      public Var(string name)
+      {
+        this._name = name;
+      }
+
+      public override bool Interpret(Context context)
+      {
+        return context.GetValue(this._name);
+      }
+    }
+
+    public class Const : AbstractExpression
+    {
+      private bool _value;
+
+      public Const(bool val)
+      {
+        this._value = val;
+      }
+
+      public override bool Interpret(Context context)
+      {
+        return this._value;
+      }
+    }
+
+    // Binary
+
+    public abstract class BinaryExpression : AbstractExpression
+    {
+      protected AbstractExpression _e1, _e2;
+
+      public BinaryExpression(AbstractExpression e1, AbstractExpression e2)
+      {
+        this._e1 = e1;
+        this._e2 = e2;
+      }
+    }
+
+    public class Or : BinaryExpression
+    {
+      public Or(AbstractExpression e1, AbstractExpression e2) : base(e1, e2) { }
+
+      public override bool Interpret(Context context)
+      {
+        return _e1.Interpret(context) || _e2.Interpret(context);
+      }
+    }
+
+    public class And : BinaryExpression
+    {
+      public And(AbstractExpression e1, AbstractExpression e2) : base(e1, e2) { }
+
+      public override bool Interpret(Context context)
+      {
+        return _e1.Interpret(context) && _e2.Interpret(context);
+      }
+    }
+    
+    // Unary
+
+    public abstract class UnaryExpression : AbstractExpression
+    {
+      protected AbstractExpression _e;
+
+      public UnaryExpression(AbstractExpression e)
+      {
+        this._e = e;
+      }
+    }
+
+    public class Not : UnaryExpression
+    {
+      public Not(AbstractExpression e) : base(e) { }
+
+      public override bool Interpret(Context context)
+      {
+        return !_e.Interpret(context);
+      }
+    }
+
+
+  }
+}
